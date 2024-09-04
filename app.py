73c7d654f4906fd11b417bf80c640ce14c35d0ba
daviyaic <124395832+daviyaic@users.noreply.github.com>
2024-09-04 09:47:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<center><h1><font color=red>Germinare Tech, EU AMO DEVOPS..</font></h1></center> '
+    return '<center><h1><font color=red>Germinare Tech, I LOVE DEVOPS..</font></h1></center> '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
